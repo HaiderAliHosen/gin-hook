91fc8d908c43780c31fed7b6a89ec45448b2435e
Haider-AliHosen <aali.raz.myself@gmail.com>
2020-12-31 16:58:45 +0600

    for news portal

--- hook/model.go
+++ hook/model.go
@@ -1,11 +1,15 @@
 package hook
 
+import "time"
+
 //Hook struct
 type Hook struct {
-	ID      string `json:"id" bson:"_id" `
-	Name    string `json:"name" bson:"name" binding:"required"`
-	Address string `json:"address" bson:"address" binding:"required"`
-	Country string `json:"country" bson:"country"`
-	Number  int64  `json:"number" bson:"number"`
-	Auth    bool   `json:"auth" bson:"auth" binding:"required"`
+	ID      string    `json:"id" bson:"_id" `
+	Title   string    `json:"title" bson:"title" binding:"required"`
+	Date    time.Time `json:"date" bson:"date" binding:"required"`
+	Content string    `json:"content" bson:"content" binding:"required"`
+	// Address string    `json:"address" bson:"address" binding:"required"`
+	// Country string    `json:"country" bson:"country"`
+	// Number  int64     `json:"number" bson:"number"`
+	Auth bool `json:"auth" bson:"auth" `
 }
